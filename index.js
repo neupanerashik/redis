720d241925df0dfd36c355fdaf1998e7509e8a9f
Rashik Neupane <neupanerashik1@gmail.com>
2024-01-20 12:49:02 +0545

    redis index

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const Redis = require("ioredis");
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+  db: process.env.REDIS_DB,
+});
+
+console.log(redis);
+
+module.exports = redis;
